fd090ba0776cd18aaa438544b874f84ac851e725
김영석 <yeongseok@gim-yeongseog-ui-MacBook-Pro.local>
2018-10-03 14:23:41 +0900

    get flickr api key

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -154,11 +154,15 @@ extension MapVC: MKMapViewDelegate {
         addSpinner()
         addProgressLbl()
         
+        
+        
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         
         let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
         mapView.addAnnotation(annotation)
+        
+        print(flickrUrl(forApikey: apiKey, withAnnotation: annotation, andNumberOfPhotos: 40))
         
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
